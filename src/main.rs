6652aa67884359707d3f7e0af6632b98a6d42b98
chrisszy123 <bassygoodluck@gmail.com>
2025-10-10 11:16:40 +0100

    learned structs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ fn main() {
     println!("{}", fib(15));
     let my_string = String::from("Hello");
     let count: usize = str_char_len(my_string);
-    println!("The string count for hello {}", count)
+    println!("The string count for hello {}", count);
+    let user:User = get_user_details();
+    println!("The user details {:?}", user);
 }
 
 fn is_even(num: i32) -> bool {
@@ -36,4 +38,23 @@ fn fib(num: i32) -> i32 {
 
 fn str_char_len(s: String) -> usize {
     s.chars().count()
+}
+
+// Structs
+#[derive(Debug)]
+struct User {
+    is_active: bool,
+    name: String,
+    email: String,
+    age: i32
+}
+
+fn get_user_details() -> User {
+    let user1:User = User {
+        is_active: true,
+        name: String::from("Bassey Goodluck"),
+        email: String::from("bassygoodluck@gmail.com"),
+        age: 29
+    };
+    user1
 }
