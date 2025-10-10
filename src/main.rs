378e3a5a083f872ca8d7d47e6a18fb9bd551ca8d
chrisszy123 <bassygoodluck@gmail.com>
2025-10-10 10:52:36 +0100

    learned rust functions, variables, strings

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    println!("Rust, funtions!");
+    println!("{}", is_even(15));
+    println!("{}", fib(15));
+    let my_string = String::from("Hello");
+    let count: usize = str_char_len(my_string);
+    println!("The string count for hello {}", count)
+}
+
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        return true;
+    }
+    return false;
+}
+
+fn fib(num: i32) -> i32 {
+    let mut first: i32 = 0;
+    let mut second: i32 = 1;
+
+    if num == 0 {
+        return first;
+    }
+    if num == 1 {
+        return 1;
+    }
+
+    for _ in 0..(num - 1) {
+        let temp: i32 = second;
+        second = second + first;
+        first = temp;
+
+    }
+    return second
+}
+
+fn str_char_len(s: String) -> usize {
+    s.chars().count()
+}
